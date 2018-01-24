7b28e150dcca21ec0fd063e0c2670f62e0608351
charlesyoung <charlesyoungwin@outlook.com>
2018-01-24 22:51:51 +0800

    90subsetII 最近状态不太好

---
+++ 90subsetII/90subsetII.py
@@ -0,0 +1,25 @@
+class Solution:
+    def subsetsWithDup(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = []
+        #nums.sort()
+        self.backtrack(res, [], nums, 0)
+        return res
+
+    def backtrack(self, res, temp, nums, start):
+        res.append(list(temp))
+        for i in range(start, len(nums)):
+            if i > start and nums[i] == nums[i - 1]:
+                continue
+            temp.append(nums[i])
+            self.backtrack(res, temp, nums, i + 1)
+            temp.pop()
+
+
+if __name__ == "__main__":
+    solu = Solution()
+    nums = [2, 3, 2]
+    print(solu.subsetsWithDup(nums))
