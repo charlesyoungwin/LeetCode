433e1884188ac9f35c3319b13055cbe0debb2809
charlesyoung <charlesyoungwin@outlook.com>
2018-01-25 23:40:50 +0800

    79题 没做出来，明天继续

---
+++ 79WordSearch/79WordSearch.py
@@ -0,0 +1,51 @@
+class Solution:
+
+    #time exceeded
+    def exist(self, board, word):
+        """
+        :type board: List[List[str]]
+        :type word: str
+        :rtype: bool
+        """
+        res = []
+        self.dfs(board, word, [], 0, res)
+        print(res)
+        return True if res else False
+
+    def dfs(self, board, word, path, index, res):
+        if index == len(word):
+            if len(path) == len(word):
+                res.append(path)
+        for i in range(index, len(word)):
+            for point in self.position(board, word[i]):
+                if not path or (self.isNeighbor(point, path[-1]) and point not in path):
+                    self.dfs(board, word, path + [point], i + 1, res)
+
+    def position(self, board, character):
+        res = []
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if board[i][j] == character:
+                    res.append([i, j])
+        return res
+
+    def isNeighbor(self, pointA, pointB):
+        if abs(pointA[0] - pointB[0]) == 1 and pointA[1] - pointB[1] == 0:
+            return True
+        elif abs(pointA[1] - pointB[1]) == 1 and pointA[0] - pointB[0] == 0:
+            return True 
+        else:
+            return False
+
+if __name__ == '__main__':
+    # board = [
+    #           ['A','B','C','E'],
+    #           ['E','F','C','S'],
+    #           ['A','D','E','E']
+    #         ]
+    board = [
+                ['a', 'b'],
+                ['c', 'd']
+            ]
+    word = "abcd"
+    print(Solution().exist(board, word))
