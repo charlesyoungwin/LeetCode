531d9ebac622c221e1718f345aa1cd66b0fe5875
charles <charlesyoungwin@outlook.com>
2018-01-25 17:30:58 +0800

    1 25 131 有个思路

--- 90subsetII/90subsetII.py
+++ 90subsetII/90subsetII.py
@@ -5,25 +5,6 @@ class Solution:
         :rtype: List[List[int]]
         """
         res = []
-<<<<<<< HEAD
-        self.dfs(nums, 0, [], res)
-        new_res = []
-        res = list(map(lambda x: sorted(x), res))
-        for item in res:
-            if item not in new_res:
-                new_res.append(item)
-        return new_res
-    
-    def dfs(self, nums, index, path, res):
-        res.append(path)
-        for i in range(index, len(nums)):
-            self.dfs(nums, i + 1, path + [nums[i]], res)
-
-
-if __name__ == '__main__':
-    nums = [4, 4, 4, 1, 4]
-    print(Solution().subsetsWithDup(nums))
-=======
         nums.sort()
         self.backtrack(res, [], nums, 0)
         return res
@@ -42,4 +23,3 @@ if __name__ == "__main__":
     solu = Solution()
     nums = [4, 4, 4, 1, 4]
     print(solu.subsetsWithDup(nums))
->>>>>>> origin/dev
