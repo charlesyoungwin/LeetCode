63d79eca6751e6a109d36160ddaac05d5e7c4a57
charles <charlesyoungwin@outlook.com>
2018-01-29 23:50:55 +0800

    2018 1 29 24题

--- 24SwapNodesInPairs/24SwapNodesInPairs.py
+++ 24SwapNodesInPairs/24SwapNodesInPairs.py
@@ -24,9 +24,19 @@ class Solution:
         head.next = self.helper(head.next)
         return newHead
 
+    #recursive modified solution
+    def swapPairsModified(self, head):
+        if not head or not head.next:
+            return head
+        tmp = head.next
+        head.next = self.swapPairsModified(tmp.next)
+        tmp.next = head
+        return tmp
+
     #solution2 
     def swapPairsV2(self, head):
-        if not head: return None
+        if not head: 
+            return None
         p1, p2 = head, head.next
         while p2:
             p1.val, p2.val = p2.val, p1.val
@@ -34,13 +44,23 @@ class Solution:
             p2 = p1.next if p1 else p1
         return head
 
+    #solution3
+    def swapPairsV3(self, head):
+        pre, pre.next = self, head
+        while pre.next and pre.next.next:
+            a = pre.next
+            b = a.next
+            pre.next, b.next, a.next = b, a, b.next
+            pre = a
+        return self.next
+
 if __name__ == '__main__':
     head = ListNode(1)
     head.next = ListNode(2)
     head.next.next = ListNode(4)
     head.next.next.next = ListNode(3)
     head.next.next.next.next = None
-    head = Solution().swapPairsV2(head)
+    head = Solution().swapPairsModified(head)
     while head:
         print(head.val, end=" ")
         head = head.next
