dfb125071ce54c8413cb66f98eed00de2369138c
charles <charlesyoungwin@outlook.com>
2018-03-05 22:40:31 +0800

    对84题增加了stack的解法

--- 84LargestRectangleInHistogram/84.py
+++ 84LargestRectangleInHistogram/84.py
@@ -43,11 +43,21 @@ class Solution:
         :type heights: List[int]
         :rtype: int
         """
-        length = len(heights)
-        stack = []
+        heights.append(0)
+        stack = [-1]
+        ans = 0
+        for i in range(len(heights)):
+            while heights[i] < heights[stack[-1]]:
+                h = heights[stack.pop()]
+                w = i - stack[-1] - 1
+                ans = max(ans, h * w)
+            stack.append(i)
+        heights.pop()
+        return ans
+
         
 
 
 if __name__ == '__main__':
     heights = [2, 1, 5, 6, 2, 3]
-    print(Solution().largestRectangleArea(heights))
+    print(Solution().largestRectangleAreaV2(heights))
